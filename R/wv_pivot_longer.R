4fe34215ea59071c80e913edaa806cb000f1be9e
Danielle Dempsey <ddempsey@perennia.ca>
2024-06-26 15:18:47 -0300

    modify current function for wave data

--- R/wv_pivot_longer.R
+++ R/wv_pivot_longer.R
@@ -21,62 +21,28 @@
 #'   sea_water_speed_m_s (WaterSpeed), and sea_water_to_direction_degree
 #'   (WaterDirection).
 #'
-#' @importFrom dplyr all_of filter left_join mutate rename select
-#' @importFrom tidyr pivot_longer pivot_wider
+#' @importFrom dplyr last_col
+#' @importFrom tidyr pivot_longer
 #'
 #' @export
 
 
-wv_pivot_longer <- function(dat_wide, rm_na = TRUE) {
+wv_pivot_longer <- function(dat, vars = NULL) {
 
-  #   # name of the timestamp column (timestamp_utc or timestamp_ns)
-  # timestamp_colname <- dat_wide %>%
-  #   select(contains("timestamp")) %>%
-  #   colnames()
-  #
-  # # pull out sensor depth measurements because there are only entries in column all_of(index)
-  # sensor_depth <- dat_wide %>%
-  #   filter(variable == "SensorDepth") %>%
-  #   select(
-  #     timestamp_foo = contains("timestamp"),
-  #     sensor_depth_below_surface_m = all_of(index)
-  #   )
-  #
-  # dat <- dat_wide %>%
-  #   filter(variable != "SensorDepth") %>%
-  #   rename(timestamp_foo = contains("timestamp")) %>%
-  #   select(-Num) %>%
-  #   # use index - 1 because dropped the Num column
-  #   pivot_longer(
-  #     cols = (all_of(index) - 1):last_col(),
-  #     names_to = "bin_id", values_to = "value"
-  #   ) %>%
-  #   pivot_wider(names_from = "variable", values_from = value) %>%
-  #   left_join(sensor_depth, by = "timestamp_foo") %>%
-  #   select(
-  #     timestamp_foo,
-  #     sensor_depth_below_surface_m,
-  #     bin_id,
-  #     sea_water_speed_m_s = WaterSpeed,
-  #     sea_water_to_direction_degree = WaterDirection
-  #   )
-  #
-  # # in case want to use after adcp_assign_altitude (preferred)
-  # if (suppressWarnings(!is.na(as.numeric(dat$bin_id[1])))) {
-  #   dat <- dat %>%
-  #     mutate(bin_height_above_sea_floor_m = as.numeric(bin_id)) %>%
-  #     select(
-  #       timestamp_foo,
-  #       sensor_depth_below_surface_m,
-  #       bin_height_above_sea_floor_m,
-  #       sea_water_speed_m_s,
-  #       sea_water_to_direction_degree
-  #     )
-  # }
-  #
-  #
-  # colnames(dat)[which(colnames(dat) == "timestamp_foo")] <- timestamp_colname
-  #
-  # dat
+  if(is.null(vars)) {
+    dat %>%
+      pivot_longer(
+        cols = 2:last_col(),
+        names_to = "variable", values_to = "value"
+      )
+  } else {
+    dat %>%
+      pivot_longer(
+        cols = vars,
+        names_to = "variable", values_to = "value"
+      )
+
+  }
+
 }
 
