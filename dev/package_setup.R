d9b2886e5eb49c598805b0efd886fae70b9b8a79
Danielle Dempsey <ddempsey@perennia.ca>
2024-09-05 13:24:25 -0300

    fix typos in package name

--- dev/package_setup.R
+++ dev/package_setup.R
@@ -34,7 +34,7 @@ use_package("viridis")
 
 # add packages (not on CRAN)
 use_dev_package("adcp", remote = "dempsey-CMAR/adcp")
-use_dev_package("qaqcmar", remote = "dempsey-CMAR/acdp")
+use_dev_package("qaqcmar", remote = "dempsey-CMAR/qaqcmar")
 
 
 # readme
