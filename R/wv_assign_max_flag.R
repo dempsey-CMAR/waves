0113c00ab7076307ac64055624871ec76c592cd6
Danielle Dempsey <ddempsey@perennia.ca>
2024-11-08 16:36:57 -0400

    initial commit

---
+++ R/wv_assign_max_flag.R
@@ -0,0 +1,121 @@
+#' Assign each observation the maximum flag from applied QC tests.
+#'
+#' @param dat Data frame in long or wide format with flag columns from multiple
+#'   quality control tests.
+#'
+#' @param qc_tests Quality control tests included in \code{dat}. Default is
+#'   \code{qc_tests = c("grossrange", "rolling_sd", "spike")}.
+#'
+#' @param return_all Logical value indicating whether to return all quality
+#'   control flag columns or only the summary columns. If \code{TRUE}, all flag
+#'   columns will be returned. If \code{FALSE}, only the summary columns will be
+#'   returned. Default is \code{TRUE}.
+#'
+#' @return Returns \code{dat} in a wide format, with a single flag column for
+#'   each variable column.
+#'
+#' @importFrom dplyr %>% all_of any_of contains everything left_join mutate
+#'   rename select
+#' @importFrom purrr pmap
+#' @importFrom stringr str_remove_all
+#' @importFrom tidyr pivot_wider
+#'
+#' @export
+
+wv_assign_max_flag <- function(dat, qc_tests = NULL, return_all = TRUE) {
+
+  if(is.null(qc_tests)) {
+    qc_tests = c("grossrange", "rolling_sd", "spike")
+  }
+
+  # use for the join and to order columns in output
+  depl_cols <- c(
+    "county",
+    "waterbody",
+    "station",
+    "lease",
+    "latitude" ,
+    "longitude" ,
+    "deployment_id",
+    "timestamp_utc",
+    "depth_trim_flag"
+  )
+
+  qc_test_cols <- wv_thresholds %>%
+    select(qc_test, variable) %>%
+    distinct() %>%
+    mutate(col_name = paste(qc_test, "flag", variable, sep = "_")) %>%
+    arrange(qc_test)
+  qc_test_cols <- qc_test_cols$col_name
+
+  # qc_test_cols <- sort(c(
+  #   qc_test_cols,
+  #   "human_in_loop_flag_dissolved_oxygen_percent_saturation",
+  #   "human_in_loop_flag_dissolved_oxygen_uncorrected_mg_per_l",
+  #   "human_in_loop_flag_salinity_psu",
+  #   "human_in_loop_flag_sensor_depth_measured_m",
+  #   "human_in_loop_flag_temperature_degree_c",
+  #   "human_in_loop_flag_value"
+  # ))
+
+
+  # save the origal data frame to join the qc_flag columns
+  if ("variable" %in% colnames(dat)) {
+    dat_og <- dat %>%
+      pivot_wider(values_from = "value", names_from = "variable")
+  } else  dat_og <- dat
+
+  # pivot dat
+  if (!("variable" %in% colnames(dat))) {
+    dat <- wv_pivot_flags_longer(dat, qc_tests = qc_tests)
+  }
+
+  # use to join and sort the columns of the output
+  var_cols <- sort(unique(dat$variable))
+
+  # use to join and sort the columns of the output
+  qc_max_cols <- paste("qc_flag", var_cols, sep = "_")
+
+  # find the maximum flag for each variable
+  dat <- dat %>%
+    mutate(
+      qc_col = pmap(select(dat, contains("flag")), max, na.rm = TRUE),
+      qc_col = unlist(qc_col),
+      qc_col = ordered(qc_col, levels = 1:4)
+    ) %>%
+    select(-contains("flag_value")) %>%
+    rename(qc_flag = qc_col) %>%
+    pivot_wider(
+      names_from = "variable",
+      values_from = c("value", "qc_flag")
+    )
+
+  colnames(dat) <- str_remove_all(colnames(dat), pattern = "value_")
+
+  if(isTRUE(return_all)) {
+
+    join_cols <- c(
+      depl_cols[which(depl_cols %in% colnames(dat_og))], var_cols)
+
+    dat <- dat_og %>%
+      left_join(dat, by = join_cols) %>%
+      select(
+        any_of(depl_cols),    # deployment columns
+        all_of(var_cols),     # variable values
+        any_of(qc_test_cols), # qc flags
+        any_of(qc_max_cols),  # max qc flags
+        everything()          # anything left
+      )
+  } else {
+
+    dat <- dat %>%
+      select(
+        any_of(depl_cols),    # deployment columns
+        all_of(var_cols),     # variable values
+        any_of(qc_max_cols),  # max qc flags
+        everything()          # anything left
+      )
+  }
+
+  dat
+}
